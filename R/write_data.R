4301ea2e93e5ee25d7f190f57418ab3972c274f5
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-05-11 18:59:36 +0200

    Implement function #2

---
+++ R/write_data.R
@@ -0,0 +1,92 @@
+
+
+#' Write Data
+#'
+#' @param x A data frame or tibble to write to disk.
+#' @param path A path where to write the data (see details).
+#' @param resource An optional resource name (used as sub folder inside path).
+#' @param file The name of the file (with extension)
+#' @param verbose If TRUE, additional messages will be sent to the console
+#'
+#' @details
+#' By default, path takes its value from the DATA_HOME environment variable.
+#' If it's not set in the environment and no value has been provided, function will be stopped (and return FALSE).
+#' If the corresponding path does not exist, it won't be created - to avoid creating data inside a container if the mount has not been done for example.
+#'
+#' Resource is an optional parameter to allow the use of sub folder inside path.
+#' If not NULL, it will be concatenated to path (and created inside path if it does not exist)
+#'
+#' @returns returns the input x invisibly, or FALSE when it fails
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' write_data(foo, file = "my_data.csv")
+#' }
+
+
+write_data <- function(x, path = Sys.getenv("DATA_HOME"), resource = NULL, file, verbose = FALSE){
+
+  # ----------------------------------------------------------------------------
+  # check parameters
+  # ----------------------------------------------------------------------------
+
+  # -- check path
+  if(path == ""){
+    message("[Warning] Path is empty, check DATA_HOME environment variable or provide a value for path argument")
+    return(FALSE)}
+
+  # -- get extension
+  extension <- tools::file_ext(file)
+  if(extension == ""){
+    message("[Warning] Can't guess extension from file - csv will be applied by default")
+    extension <- "csv"}
+
+
+  # ----------------------------------------------------------------------------
+  # check target path
+  # ----------------------------------------------------------------------------
+
+  # -- check base path
+  if(!dir.exists(path)){
+    message("[Warning] Path does not exist - can't save the data")
+    return(FALSE)}
+
+  # -- check resource path
+  if(!is.null(resource)){
+
+    # -- concatenate
+    path <- file.path(path, resource)
+
+    if(verbose)
+      cat("[write_data] Update path with resource folder \n")
+
+    # -- check new path
+    if(!dir.exists(path))
+      dir.create(path)}
+
+
+  # ----------------------------------------------------------------------------
+  # write the data
+  # ----------------------------------------------------------------------------
+
+  # -- case csv (default)
+  if(extension == "csv"){
+
+    tryCatch({
+
+      cat("Iker is writing your data... \n")
+
+      # -- expression
+      readr::write_delim(x = x,
+                         file = file.path(path, file),
+                         delim = ";")},
+
+      # -- error
+      error = function(e){
+        message(e)
+        return(FALSE)})
+
+  }
+
+}
