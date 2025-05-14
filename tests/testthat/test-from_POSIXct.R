0dd93e5fcc9b74284716b413230b65187c3353d4
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-05-14 16:20:13 +0200

    Partial #13

--- tests/testthat/test-from_POSIXct.R
+++ tests/testthat/test-from_POSIXct.R
@@ -5,8 +5,10 @@ test_that("from_POSIXct works", {
   # tests
   # ----------------------------------------------------------------------------
 
+  datetime <- Sys.time()
+
   # -- convert POSIXct to ISO-8601
-  x <- from_POSIXct(data.frame(a = as.POSIXct("2025-05-13 12:15:30")))
+  x <- from_POSIXct(data.frame(a = as.POSIXct("2025-05-13 12:15:30", tz = "CET")))
   expect_equal(as.character(x$a), "2025-05-13T12:15:30+0200")
 
   # -- test without POSIXct (should not alter Date)
