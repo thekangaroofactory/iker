5a5eb3dfe526b3491958634f5632a51ff54afe99
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-12 19:31:35 +0200

    Fix attempt #26

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -9,9 +9,10 @@
 # "/Users/runner/work/iker/iker/check/iker.Rcheck/tests/testthat"
 # While package directory is:
 # "/Users/runner/work/iker/iker/check/iker.Rcheck/iker"
-testdata_path <- file.path(system.file("tests", "testthat", package = "iker"), "testdata")
+testdata_path <- system.file("tests", "testthat", package = "iker")
 if(testdata_path == "")
   testdata_path <- file.path(dirname(system.file(package = "iker"), "tests", "testdata"))
+testdata_path <- file.path(testdata_path, "testdata")
 
 str(testdata_path)
 str(getwd())
