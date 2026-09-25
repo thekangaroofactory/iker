44e35524a9bd409d099558038b111a1f23076328
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-25 09:36:50 +0200

    Fixed #29

--- tests/testthat/test-from_POSIXct.R
+++ tests/testthat/test-from_POSIXct.R
@@ -5,8 +5,12 @@ test_that("from_POSIXct works", {
   # tests
   # ----------------------------------------------------------------------------
 
+  # -- detect OS
+  # there is a problem on Linux where timezone uses region/country
+  tz <- if(Sys.info()[['sysname']] == "Linux") "Europe/Paris" else "CET"
+
   # -- convert POSIXct to ISO-8601
-  x <- from_POSIXct(data.frame(a = as.POSIXct("2025-05-13 12:15:30", tz = "CET")))
+  x <- from_POSIXct(data.frame(a = as.POSIXct("2025-05-13 12:15:30", tz = tz)))
   expect_equal(as.character(x$a), "2025-05-13T12:15:30+0200")
 
   # -- test without POSIXct (should not alter Date)
